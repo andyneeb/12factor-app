7ae1cbfd31751cbc5678c74323b246ed2805353a
andyneeb <andreas.neeb@gmail.com>
2016-11-23 18:41:43 +0100

    Update 00_setup.sh

--- 00_setup.sh
+++ 00_setup.sh
@@ -1,3 +1,3 @@
-oc login https://ocp-master.example.com:8443 --insecure-skip-tls-verify=true -u dev -p redhat 
+oc login --insecure-skip-tls-verify=true
 oc new-project 12factor-dev
 oc new-build --binary --name=my12factorapp
