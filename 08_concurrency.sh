207187e80aee90a97df9b5837880f03e7278bf31
Andreas Neeb <aneeb@redhat.com>
2017-01-19 17:52:48 +0100

    minor changes

--- 08_concurrency.sh
+++ 08_concurrency.sh
@@ -1,3 +1,3 @@
 oc scale dc/my12factorapp --replicas=3
 echo "App Scaled to three instances"
-while true; do curl -m 1 http://12factorappdemo.ocp.192.168.122.101.xip.io/api/hello/Master ; echo; sleep 1; done
+while true; do curl -m 1 http://12factorappdemo.ocp.192.168.122.100.xip.io/api/hello/Master ; echo; sleep 1; done
